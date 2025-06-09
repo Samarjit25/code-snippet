6fcb2279c08e048a57f58e6b210c171a3f632d04
hiarun01 <hiarun02@gmail.com>
2025-06-09 22:46:57 +0530

    add how works section

--- src/components/pages/Home.jsx
+++ src/components/pages/Home.jsx
@@ -1,8 +1,10 @@
 import HeroSection from "../HeroSection";
+import HowWorks from "../HowWorks";
 const Home = () => {
   return (
     <div className="bg-white">
       <HeroSection />
+      <HowWorks />
     </div>
   );
 };
